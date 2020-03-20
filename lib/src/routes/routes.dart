a63c8279f864210b66286cbe682472d5cde3f287
YohanJavierMamani <ymamani@exact.com.pe>
2020-03-20 10:47:28 -0500

    subir cambios usuarios

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tramiteapp/src/Vistas/Home/HomePage.dart';
 import 'package:tramiteapp/src/Vistas/Login/loginPage.dart';
+import 'package:tramiteapp/src/Vistas/Usuarios/principalPage.dart';
 
 
 Map<String, WidgetBuilder> getAplicationRoutes() {
   return <String, WidgetBuilder>{
     '/login': (BuildContext context) => LoginPage(),
     '/principal-admin': (BuildContext context) => HomePage(),
+    '/generar-envio': (BuildContext context) => PrincipalPage(),
   };
 }
+  
